110e8414b397a4eb0f78e42c0e2fb48051e7793f
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2023-07-09 13:47:01 -0400

    Refactor library.js tests

--- src/library.js
+++ src/library.js
@@ -10,13 +10,49 @@ function createLibrary(name) {
   return library
 }
 
-function addBook() {
+function addBook(library, book) {
+  if (book.genre === "fantasy") {
+    library.shelves.fantasy.push(book)
+  } else if (book.genre === "nonFiction") {
+    library.shelves.nonFiction.push(book)
+  }
+  return library
+}
 
+
+
+function checkoutBook(library, bookTitle, genre) {
+  // console.log('LIBRARY', library)
+  // console.log('BOOK', bookTitle)
+  // console.log('GENRE', genre)
+  // console.log("bOOks:", books)
+
+  var bookFound = false
+  for (let i = 0; i < library.shelves[genre].length; i++) {
+    if (library.shelves[genre][i].title === bookTitle) {
+      library.shelves[genre].splice(i, 1)
+      bookFound = true
+    }
+  }
+  // console.log(bookFound)
+  if (bookFound === true) {
+    return `You have now checked out ${bookTitle} from the ${library.name}.`
+  } else {
+    return `Sorry, there are currently no copies of ${bookTitle} available at the ${library.name}.`
+  }
+}
+
+function takeStock(library, genre) {
+  if (!genre) {
+    return `There are a total of ${Object.values(library.shelves).length} books at the ${library.name}.`
+  } else {
+    return `There are a total of ${library.shelves[genre].length} ${genre} books at the ${library.name}.`
+  }
 }
 
 module.exports = {
   createLibrary,
   addBook,
-  // checkoutBook,
-  // takeStock
+  checkoutBook,
+  takeStock
 };
